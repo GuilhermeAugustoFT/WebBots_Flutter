b65a6715612c3d0337684f0c10090462c201b247
Guilherme Teixeira <49246009+GuilhermeAugustoFT@users.noreply.github.com>
2021-05-06 14:58:33 -0300

    Remoção do aplicativo não funcional

--- 19174_19188-FlutterRobo/Flutter/app/lib/main.dart
+++
@@ -1,9 +0,0 @@
-import 'package:app/app_widget.dart';
-import 'package:app/pages/home.dart';
-import 'package:flutter/material.dart';
-import 'package:app/pages/home.dart';
-import 'package:app/app_widget.dart';
-
-void main() {
-  runApp(AppWidget());
-}
